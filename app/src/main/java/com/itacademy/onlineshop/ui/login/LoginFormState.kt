e7130635c8b14bb596615b8535c9d626750bebf5
Temirlannn <93139917+Temirlannn@users.noreply.github.com>
2022-04-16 15:50:58 +0600

    Initial commit

---
+++ app/src/main/java/com/itacademy/onlineshop/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.itacademy.onlineshop.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
---
+++ app/src/main/java/com/itacademy/onlineshop/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.itacademy.onlineshop.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
